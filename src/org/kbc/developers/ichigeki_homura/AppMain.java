32f5e007673911c8f9c5e26937ae5f20b2090ce2
sakuramilk <c.sakuramilk@gmail.com>
2013-05-25 02:43:33 +0900

    Zip読み込み処理追加、UI暫定

--- src/org/kbc/developers/ichigeki_homura/AppMain.java
+++ src/org/kbc/developers/ichigeki_homura/AppMain.java
@@ -1,19 +1,16 @@
 package org.kbc.developers.ichigeki_homura;
 
 import org.kbc.developers.ichigeki_homura.ui.MainFrame;
-import javax.swing.JButton;
-import java.awt.BorderLayout;
 
 public class AppMain {
 	
 	/**
+	 * アプリケーションのエントリポイント
+	 * 
      * @param args
      */
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
-        
-        JButton btnNewButton = new JButton("Exec");
-        mainFrame.getContentPane().add(btnNewButton, BorderLayout.SOUTH);
         mainFrame.setVisible(true);
     }
 }
