627cd8d09c665bb1c31dbb76a481773676d03214
ma34s <ma34s.sss@gmail.com>
2013-05-24 20:56:19 +0900

    add BuildProp Test

--- src/org/kbc/developers/ichigeki_homura/test/BuildPropTest.java
+++ src/org/kbc/developers/ichigeki_homura/test/BuildPropTest.java
@@ -1,5 +1,6 @@
 package org.kbc.developers.ichigeki_homura.test;
 
+import org.kbc.developers.ichigeki_homura.util.BuildProp;
 import org.kbc.developers.ichigeki_homura.util.Log;
 import org.kbc.developers.ichigeki_homura.util.TextFile;
 
@@ -8,17 +9,22 @@ public class BuildPropTest implements Testable{
 	private	static final String TAG="BuildPropTest";
 
 	TextFile mTextFile;
-
+	BuildProp mBuildProp;
 	public BuildPropTest()
 	{
 		mTextFile =  new TextFile("Test/build.prop", TextFile.EUC_JP);
+		mBuildProp = new BuildProp(mTextFile);
 	}
+
 
 	@Override
 	public void run() {
 		Log.d(TAG,"Start Test");
 
-
+		Log.d(TAG,"API Level = "+ mBuildProp.getApiLevel());
+		Log.d(TAG,"ProductDevice = "+ mBuildProp.getProductDevice());
+		Log.d(TAG,"ProductName = "+ mBuildProp.getProductName());
+		Log.d(TAG,"all props :\n"+ mBuildProp.toString());
 
 		Log.d(TAG,"End Test");
 	}
