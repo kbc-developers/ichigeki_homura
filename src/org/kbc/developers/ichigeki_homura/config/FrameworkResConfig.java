a87e14065e7c612f3b448b74f9b0cc924efc5b94
ma34s <ma34s.sss@gmail.com>
2013-06-10 14:02:58 +0900

    add Config for Framework edit

--- src/org/kbc/developers/ichigeki_homura/config/FrameworkResConfig.java
+++ src/org/kbc/developers/ichigeki_homura/config/FrameworkResConfig.java
@@ -2,6 +2,7 @@ package org.kbc.developers.ichigeki_homura.config;
 
 import java.util.ArrayList;
 
+import org.kbc.developers.ichigeki_homura.util.Prop;
 import org.kbc.developers.ichigeki_homura.util.PropMultiItem;
 import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
@@ -10,30 +11,26 @@ import org.simpleframework.xml.Root;
 
 
 @Root
-public class Configs implements java.io.Serializable{
+public class FrameworkResConfig implements java.io.Serializable{
 
 	/**
 	 *
 	 */
 	private static final long serialVersionUID = -3019800764906699358L;
 
-	@Element
-	public String name;
-	@Element
-	public String test;
 
 	@Element
-	public UpdeterConfigs updater;
-
+	public boolean isDecode;
 	@Element
-	public FrameworkResConfig framework;
+	public String arrays_xml_path;
+	@ElementList
+	public ArrayList<Prop> add_items;
 
-	public Configs()
+	public FrameworkResConfig()
 	{
-		name="";
-		test="";
-		updater = new UpdeterConfigs();
-		framework = new FrameworkResConfig();
+		isDecode = false;
+		arrays_xml_path = "";
+		add_items = new ArrayList<Prop>();
 	}
 }
 
--- src/org/kbc/developers/ichigeki_homura/config/samsung/Configs_sc02e.java
+++ src/org/kbc/developers/ichigeki_homura/config/samsung/Configs_sc02e.java
@@ -15,6 +15,9 @@ import org.simpleframework.xml.Root;
 @Root
 public class Configs_sc02e extends Configs implements java.io.Serializable{
 
+	//----------------------------------------------------------
+	//Update Script
+	//----------------------------------------------------------
 	private static final Prop[] ADD_PROP = {
 		new Prop(UpdateScript.BUILD_PRODUCT,"sc02e"),
 		new Prop(UpdateScript.BUILD_PRODUCT,"SC-02E"),
@@ -22,16 +25,41 @@ public class Configs_sc02e extends Configs implements java.io.Serializable{
 		new Prop(UpdateScript.PRODUCT_DEVICE,"SC-02E"),
 	};
 
+	//----------------------------------------------------------
+	//Framework
+	//----------------------------------------------------------
+	private static final String ARRAYS_XML_PATH="";
+	private static final Prop[] ARRAYS_PROP = {
+		new Prop("/resources/string-array[@name='config_statusBarIcons']/item","felica_lock"),
+	};
 
 	public Configs_sc02e()
 	{
 		super();
 		name="sc02c";
 		test="sc02c_test";
+		//----------------------------------------------------------
+		//File Copy
+		//----------------------------------------------------------
 
+
+		//----------------------------------------------------------
+		//Update Script
+		//----------------------------------------------------------
 		for(int i=0;i<ADD_PROP.length;i++)
 		{
 			updater.add_prop.add(ADD_PROP[i]);
+		}
+
+		//----------------------------------------------------------
+		//Framework
+		//----------------------------------------------------------
+		framework.isDecode = true;
+		framework.arrays_xml_path = ARRAYS_XML_PATH;
+
+		for(int i=0;i<ADD_PROP.length;i++)
+		{
+			framework.add_items.add(ARRAYS_PROP[i]);
 		}
 
 	}
